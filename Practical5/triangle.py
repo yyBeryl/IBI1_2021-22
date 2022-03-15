a73821fe8e51075e48d4244756bd6ecef7023078
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-15 17:22:38 +0800

    triangle.py

---
+++ Practical5/triangle.py
@@ -0,0 +1,18 @@
+#a=1.
+#b=1
+#b=b+1
+#a+=b
+#print(a)
+#repeat for b in range(1,11)
+	#run
+	#Is b out of range?
+	#	If less than 10:Keep running
+	#	If 10:DONE!
+a=1
+b=1
+while 1==1:
+	print(a)
+	b=b+1
+	a+=b
+	if b==11:
+		break
