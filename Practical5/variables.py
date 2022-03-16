7a1a978fd7a8fa242aa1164e30cb1ed8e9c6df9e
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-16 14:25:50 +0800

    variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,24 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+if d>e:
+	print("d is greater and the rate of new cases is greater in 2020")
+else:
+	print("e is greater and the rate of new cases is greater in 2021")
+#e is greater and the rate of new cases is greater in 2021
+X=True
+Y=True
+W=X and Y
+print W
+
+X=False
+Y=False
+W=X and Y
+print W
+
+X=True
+Y=False
+W=X and Y
+print W
