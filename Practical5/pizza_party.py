d817088cb5a5b5e1d06ba76fccd0cea4f7565591
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-18 15:40:30 +0800

    pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,7 +1,9 @@
 #p is the number of pieces
 #n is the number of cuts
 #p==(n**2+n+2)/2
-#print(p pieces of pizza can be cut for n cuts)
+#This formula was obtained from the practical 5 provided by my teacher
+#the final line print(p pieces of pizza can be cut for n cuts)
+#I had help from a fellow student in class to know how to use "str" to print figures and text together.
 #repeat until p>=64
 
 n=1
