ee4ac3762ac55689a7e8a6e3d54a632ac54f7fa4
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-18 15:51:54 +0800

    triangle.py

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,9 +1,11 @@
+#a is the number of points in the triangle
+#b is the added number of points each time
 #a=1
 #b=1
 #b=b+1
-#a+=b
+#a+=b by doing this I can get the number of points each time 
 #print(a)
-#repeat for b in range(1,11)
+#repeat for b in range(1,11) so I can get ten statistics
 	#run
 	#Is b out of range?
 	#	If less than 10:Keep running
