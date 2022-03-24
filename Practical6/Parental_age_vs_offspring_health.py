4357e7f14c9d4003a9d0b35f6ed5c65e0668885a
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-24 20:18:57 +0800

    Parental

--- Practical6/Parental_age_vs_offspring_health.py
+++ Practical6/Parental_age_vs_offspring_health.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 #create the data I need
 x=[30,35,40,45,50,55,60,65,70,75]
+#x is parental age
 y=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
+#y is  the relative risk for congenital heart disease in the offspring 
 #create a figure
 fig=plt.figure(1)
 plt.scatter(x,y,marker='o')
