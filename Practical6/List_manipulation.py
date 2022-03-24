0bdb704d73d9aa931feb45b7de74092c9c7c7add
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-24 20:23:57 +0800

    List

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #input my data
 x=[45,36,86,57,53,92,65,45]
-#set parameters
+#set parameters and save it in "x"
 bp=plt.boxplot(x,
 	notch=False,
 	#show in Non-concave form
@@ -16,14 +16,16 @@ bp=plt.boxplot(x,
 	boxprops={'color':'green'}
 	#Outliers are shown in red
 	)
+#name the title and x axis
 plt.title('List manipulation')
 plt.xlabel('marks')
 plt.show()
 #write sth. to calculate whether Rob passes IBI
-s=sum(x)
-m=s/8
+#caculate the mean of marks
+m=sum(x)/len(x)
+#I learned this way from this website https://www.jb51.net/article/176500.htm
 if m>=60:
-	print("With a score of 60 or more, Rob passed the IBI course.")
+	print("With a score of 60 or more, Rob passed the IBI course!")
 else:
 	print("With a score lower than 60, Rob didn't pass the IBI course.")
 
