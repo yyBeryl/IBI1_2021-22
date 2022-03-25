ff2086f308058186cc9b7a3567eb83a0ea8917d6
Beryl <beryl@BeryldeMacBook-Air.local>
2022-03-25 09:52:26 +0800

    List

--- Practical6/List_manipulation.py
+++ Practical6/List_manipulation.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #input my data
+#x is marks
 x=[45,36,86,57,53,92,65,45]
-#set parameters and save it in "x"
+#set parameters
 bp=plt.boxplot(x,
 	notch=False,
 	#show in Non-concave form
@@ -20,9 +21,14 @@ bp=plt.boxplot(x,
 plt.title('List manipulation')
 plt.xlabel('marks')
 plt.show()
-#write sth. to calculate whether Rob passes IBI
+#to print the sorted values of marks
+#save these sorted values in "y"
+y=sorted(x)
+print("Rob's marks across the semester are "+str(y))
+#to calculate whether Rob passes IBI
 #caculate the mean of marks
 m=sum(x)/len(x)
+print("Rob's average mark of the IBI course in this semester is "+str(m))
 #I learned this way from this website https://www.jb51.net/article/176500.htm
 if m>=60:
 	print("With a score of 60 or more, Rob passed the IBI course!")
