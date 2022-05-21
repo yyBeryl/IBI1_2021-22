9d463c8e2c87bdb2d6e8d6f7587a9b4e3fdc2f3e
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-21 23:58:08 +0800

    Modify fragment_counter.py

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -1,6 +1,7 @@
-seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
-n=seq.find('GAATTC')
-m=n+1
+seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'#Input a sequence
+n=seq.find('GAATTC')#n is the number of GAATTC in seq
+m=n+1#m is the number of fragments generated
+#Check whether the script contains the variable seq
 if n >= 0:
     print('The sequence would be cut to '+m+' fragments')
 else:
