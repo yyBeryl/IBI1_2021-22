52c1841ea6a8cad247e46c9abd3a8d156b1b6dd9
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-22 00:22:55 +0800

    modify get_cut_genes.py

--- Practical8/get_cut_genes.py
+++ Practical8/get_cut_genes.py
@@ -4,7 +4,7 @@ seq = open('sequence.fa','r')#read the original fasta file
 temporary=" "#create a blank string to store sequences temporarily
 xfile = open('cut_genes.fa','w')#create a new file cut_gene.fa
 for line in seq:
-#read line without the first line
+#read line without the line containing the name of genes
     line = seq.readline()
     line = line.strip()
     if line.startswith('>'):
@@ -15,5 +15,5 @@ for line in seq:
         continue
     temporary+=line
 if 'GAATTC' in temporary:
-    xfile.write(temporary+'\n')
+    xfile.write(temporary+'\n')#write the target sequences to the x file
 xfile.close()
