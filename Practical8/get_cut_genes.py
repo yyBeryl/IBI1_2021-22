2630fe80a8f187928acf75b907fe31ea35c4fce8
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-22 00:03:56 +0800

    modify get_cut_genes.py

--- Practical8/get_cut_genes.py
+++ Practical8/get_cut_genes.py
@@ -1,15 +1,17 @@
 import os
 os.chdir("/Users/beryl/IBI1_2021-22/Practical8")
-seq = open('sequence.fa','r')
-temporary=" "
-xfile = open('cut_genes.fa','w')
+seq = open('sequence.fa','r')#read the original fasta file
+temporary=" "#create a blank string to store sequences temporarily
+xfile = open('cut_genes.fa','w')#create a new file cut_gene.fa
 for line in seq:
+#read line without the first line
     line = seq.readline()
     line = line.strip()
     if line.startswith('>'):
+	#write sequences that can be cut in xfile
         if 'GAATTC' in temporary:
             xfile.write(temporary+'\n')
-        temporary=" "
+        temporary=" "#empty the string each time
         continue
     temporary+=line
 if 'GAATTC' in temporary:
