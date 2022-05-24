eacf215fb45912fa23c12e9c523c8092f31876a6
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-24 21:51:06 +0800

    add some pseudocode

--- Practical10/Chocolate_bar_affordability_calculator.py
+++ Practical10/Chocolate_bar_affordability_calculator.py
@@ -1,9 +1,9 @@
 class Chocolate():
-    def __init__(self,total_money,single_price):
+    def __init__(self,total_money,single_price):#define the funciton
 #take two needed parameters
         self.total_money=total_money
         self.single_price=single_price
-    def cal(self):
+    def cal(self):#define the function
         b=self.total_money//self.single_price#calculate how many chocolate bars can buy
         c=self.total_money%self.single_price#calculate how many money can be back
         print(self.total_money,"can buy",b,"bars of chocolate with",c,"back.")      
