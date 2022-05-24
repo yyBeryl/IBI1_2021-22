fdc9f145d9bba74bdb2df1abb6f26ca228f018cf
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-24 21:41:08 +0800

    add some pseudocode

--- Practical8/count_cut_genes.py
+++ Practical8/count_cut_genes.py
@@ -2,14 +2,14 @@ import os
 os.chdir("/Users/beryl/IBI1_2021-22/Practical8")
 seq = open('sequence.fa','r')
 #accept a new output of file name
-filename=input("Please print the filename.")
+filename=input("Please enter the filename:")
 yfile = open(filename+'.fa','w')#yfile is used for storage of the name of the gene and the number of cuts and the DNA sequence
 fragment=" "#used for store sequence temporarily
 for line in seq:
     line = seq.readline()
 #read line without the line of the sequence information
     line = line.strip()
-    if line.startswith('>')
+    if line.startswith('>'):
         if 'GAATTC' in fragment:
 #get the name of the gene
             for i in range(len(line)):
@@ -20,11 +20,11 @@ for line in seq:
                         name+=line[n]
                         n=n+1
             m=fragment.find('GAATTC')#m is the number of cuts
-            yfile.write(name+' '+str(m)+'\n'+fragment+'\n')
+            yfile.write('>'+name+' '+str(m)+'\n'+fragment+'\n')
         fragment=" "#empty each time
         continue
     fragment+=line
-#add the possible overlooked situation
+#add the possible overlooked situation(the last line)
 if 'GAATTC' in fragment:
     for i in len(line):
         if "gene:"==line[i,i+5]:
