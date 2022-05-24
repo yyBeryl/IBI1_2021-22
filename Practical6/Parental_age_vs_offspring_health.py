8b97f38e57d6e620429c978b38ff471b120613a1
Beryl <beryl@BeryldeMacBook-Air.local>
2022-05-24 11:22:02 +0800

    modify according to the feedback

--- Practical6/Parental_age_vs_offspring_health.py
+++ Practical6/Parental_age_vs_offspring_health.py
@@ -3,11 +3,19 @@ PC={30:1.03,35:1.07,40:1.11,45:1.17,50:1.23,55:1.32,60:1.42,65:1.55,70:1.72,75:1
 import random
 #x is parental age
 x=[30,35,40,45,50,55,60,65,70,75]
-#randomly choose one value from the parental age to simulate an imput from the user
-a=random.choice(x)
+#input one value from the parental age to simulate an imput from the user
+#display the frequency table
+dict={a:PC[a]}
 #I searched the Internet to learn how to do this
 #the website is https://www.jb51.net/article/199816.htm
-print('The risk for CHD in the offspring is '+str(PC[a])+' of a father of an imput age')
+print('The risk for CHD in the offspring is '+str(PC[a])+' of a father of '+str(a)+'.')
+print(dict)
+#take 30 as an example
+example=30
+dict={example:PC[example]}
+print('The risk for CHD in the offspring is '+str(PC[a])+' of a father of '+str(example)+'.')
+print(dict)
+
 import matplotlib.pyplot as plt
 import numpy as np
 #create the data I need
